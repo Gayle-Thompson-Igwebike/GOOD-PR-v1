705957ed3ac457f178985289e06dae2eaff0289c
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-06 12:40:10 +0100

    get PRs functions update

--- server/getPRCount.js
+++ server/getPRCount.js
@@ -1,44 +1,58 @@
 import { Router } from "express";
-import axios from "axios"; 
+import axios from "axios";
 const router = Router();
 
+// (api request to get all collaborators PRs including closed prs is:  https://api.github.com/repos/${repoOwner}/${repoName}/pulls?state=all
 
-// (git api request format to get all collaborators PRs)
+//example using our goodpR-V1- repo below:
 
-const apiUrl = `https://api.github.com/repos/Gayle-Thompson-Igwebike/GOOD-PR-v1/pulls`;
+const PullRequestsApiUrl = `https://api.github.com/repos/Gayle-Thompson-Igwebike/GOOD-PR-v1/pulls?state=all`;
 
+export const getAllTeamMembersPRs = router.get("/", (req, res) => {
+  const repoOwner = "Gayle-Thompson-Igwebike";
+  const repoName = "GOOD-PR-v1";
 
-//(git api request format to get a single users PRs)
+  axios
+    .get(PullRequestsApiUrl)
+    .then((response) => {
+      const pullRequestCount = response.data.length;
 
-const singleUserapiUrl = `https://api.github.com/search/issues?q=is:pr+repo:Gayle-Thompson-Igwebike/GOOD-PR-v1/+author:Gayle-Thompson-Igwebike`
+      if (!response.data) {
+        res.send("An error has occurred: " + response.data.message);
+      } else {
+        res.status(200).json({
+          message: `Total Number of pull request contributions from this repo is: ${pullRequestCount}`,
+        });
+      }
+      res.status(200).json({ pullRequestCount: pullRequestCount });
+    })
+    .catch((error) => {
+      console.error("Error fetching pull requests:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
 
+export const getUserPRs = router.get("/:username", (req, res) => {
+  const repoOwner = "Gayle-Thompson-Igwebike";
+  const repoName = "GOOD-PR-v1";
+  const author = req.params.username; // Get the individual GitHub username from the route parameter
 
- export const getPRCount = router.get("/", (req, res) => {
-// variables to hold username and repository name on GitHub.
-   const repoOwner = "Gayle-Thompson-Igwebike";
-   const repoName = "Affirmation-page";
+  const singleUserapiUrl = `https://api.github.com/search/issues?q=is:pr+repo:${repoOwner}/${repoName}/+author:${author}`;
 
-   axios
-     .get(singleUserapiUrl)
-     .then((response) => {
-       const pullRequestCount = response.data.length;
+  axios
+    .get(singleUserapiUrl)
+    .then((response) => {
+      const pullRequests = response.data.items;
+      const pullRequestCount = pullRequests.length;
 
-       if (!response.data) {
-         res.send("An error has occurred: " + response.data.message);
-       } else {
-         res.status(200).json({ message: "Here is the requested data" });
-       }
+      console.log(
+        `Total Number of pull request contributions by ${author} in this repo is: ${pullRequestCount}`
+      );
 
-       console.log(
-         `Total Number of pull request contributions from ${repoOwner} in this repo is: ${pullRequestCount}`
-       );
-
-       
-       res.status(200).json({ pullRequestCount: pullRequestCount });
-     })
-     .catch((error) => {
-       console.error("Error fetching pull requests:", error);
-       res.status(500).json({ error: "Internal server error" });
-     });
- });
-
+      res.status(200).json({ pullRequestCount });
+    })
+    .catch((error) => {
+      console.error("Error fetching pull requests:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
